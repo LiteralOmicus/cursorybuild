ac1a11a5dd9a47596fe6fa1108d498cf47152ef9
LiteralOmicus <115115110+LiteralOmicus@users.noreply.github.com>
2025-12-22 20:38:45 -1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1659,7 +1659,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 String saveuserName = _dump["info"]?["handle"] ?? "N/A"; //_dump["info"]["handle"];
                 //int saveuserexp = _dump["info"]["exp"];
                 // List privet = _dump["Statuses"];
-                #ANONBLACK
+                //ANONBLACK
                List icon = _dump["info"]?["photo"] ??  ["4", "UP"]; //_dump["info"]["photo"];
                 //context.read<Referencer>().setPic(icon);
                 //final icon = context.read<Referencer>().returnPic();
