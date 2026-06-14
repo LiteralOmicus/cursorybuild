4d2faf674472590b17a7b984d6b3a70482c36b14
LiteralOmicus <115115110+LiteralOmicus@users.noreply.github.com>
2026-06-14 00:10:52 -1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -893,8 +893,7 @@ class Referencer extends ChangeNotifier {
       notifyListeners();
     }
   }
-}
-}
+
   //CAUSE FOR CONCERN
   
   // 2. This is the function you requested.
@@ -2699,7 +2698,7 @@ Future<List<String>> fetchSpecificResource(BuildContext context, String resource
       
     } else {
       // SCENARIO B: We are completely idle. Open the file picker!
-      FilePickerResult? result = await FilePicker.platform.pickFiles(
+      FilePickerResult? result = await FilePicker.pickFiles(
         type: FileType.custom,
         allowedExtensions: ['pdf'], 
       );
